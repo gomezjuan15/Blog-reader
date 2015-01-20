c615a0dac2c6936b595172eacc427747ac07feca
Juan Gomez <erioly@gmail.com>
2015-01-20 01:38:00 -0500

    add json extraction

--- app/src/main/java/com/treehouse/gaara/blogreader/MainListActivity.java
+++ app/src/main/java/com/treehouse/gaara/blogreader/MainListActivity.java
@@ -2,34 +2,48 @@ package com.treehouse.gaara.blogreader;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainListActivity extends ListActivity {
 
-//    protected String [] mAndroidNames = {
-//            "Item 1",
-//            "Item2"
-//    };
-
     protected String [] mAndroidNames;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
+        if(isNetworkAvailable()) {
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
-
+            GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+            getBlogPostsTask.execute();
+        } else {
+            Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
+        }
 //        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
 
@@ -54,5 +68,64 @@ public class MainListActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean isNetworkAvailable() {
+
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String>  {
+
+        @Override
+        protected String doInBackground(Object[] params) {
+
+            int responseCode = -1;
+
+            try {
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+
+                if(responseCode == HttpURLConnection.HTTP_OK) {
+
+                    InputStream inputStream = connection.getInputStream();
+                    Reader reader = new InputStreamReader(inputStream);
+                    int contentLength = connection.getContentLength();
+                    char[] charArray = new char[contentLength];
+                    reader.read(charArray);
+                    String responseData = new String(charArray);
+
+                    JSONObject jsonResponse = new JSONObject(responseData);
+                    String status = jsonResponse.getString("status");
+                    Log.v(TAG, "Status: " + status);
+
+                    JSONArray posts = jsonResponse.getJSONArray("posts");
+
+                    for(int i = 0; i < posts.length(); i++) {
+                        JSONObject post = posts.getJSONObject(i);
+                        String title = post.getString("title");
+                        Log.v(TAG, "Post " + i + ": " + title);
+                    }
+
+                } else {
+                    Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+                }
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "Exception caught: " + e);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception caught: " + e);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception caught: " + e);
+            }
+
+            return "Code: " + responseCode;
+        }
     }
 }
